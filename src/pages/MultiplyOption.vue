4ba34b9d97ad57d88cac14bc9b00d86530fe6140
Rodrigo Rangel <rodrigorangeln@gmail.com>
2023-01-13 13:51:13 -0300

    Refactoring

--- src/pages/MultiplyOption.vue
+++ src/pages/MultiplyOption.vue
@@ -14,79 +14,72 @@
         </div>
       </q-card-section>
       <q-separator inset size="0.3rem" color="dark" />
-      <q-card-section ref="msg" class="text-center">
-        {{ msg }}
+      <q-card-section class="text-center" :class="selected ? 'text-h3' : ''">
+        {{ selected ? selected : msg }}
       </q-card-section>
       <tr class="row text-h2 text-weight-bold justify-between q-pa-md">
-        <!-- <td v-for="option in options" :key="option">
-          {{ option }}
-        </td> -->
-        <td @click="selected(options[0])">{{ options[0] }}</td>
-        <td>{{ options[1] }}</td>
-        <td>{{ options[2] }}</td>
-      </tr>
-      <tr class="row text-h2 text-weight-bold justify-between q-pa-md">
-        <td>{{ options[3] }}</td>
-        <td>{{ options[4] }}</td>
-        <td>{{ options[5] }}</td>
+        <div
+          v-for="(option, index) in options"
+          :key="index"
+          class="col-4 text-center"
+        >
+          <q-btn
+            :class="
+              selected == option ? 'animate__animated animate__shakeX' : ''
+            "
+            @click="onSelected(option)"
+            size="0.9em"
+            flat
+            >{{ option }}</q-btn
+          >
+        </div>
       </tr>
     </q-card>
   </div>
 </template>
 
-  <script>
-import { ref, defineComponent } from 'vue';
+  <script setup>
+import { ref } from 'vue';
+import 'animate.css';
 
-export default defineComponent({
-  name: 'MultiplyOption',
+const random = (param = 10) => {
+  let randomNumber = Math.floor(Math.random() * param) + 2;
+  return randomNumber;
+};
 
-  setup() {
-    const random = (param = 10) => {
-      let randomNumber = Math.floor(Math.random() * param) + 2;
-      return randomNumber;
-    };
-
-    let number = ref({
-      a: random(),
-      b: random(),
-    });
-    let answer = ref(number.value.a * number.value.b);
-    let options = ref([]);
-    let msg = ref('Escolha sua resposta').value;
-
-    for (var i = 0; i < 5; i++) {
-      options.value.push(distractors(i));
-    }
-    options.value.push(answer.value);
-    shuffleArray(options.value);
-
-    function distractors(i) {
-      if (i < 3) {
-        return answer.value + random();
-      } else {
-        return answer.value - random();
-      }
-    }
-
-    function shuffleArray(array) {
-      for (let i = 0; i < array.length; i++) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-      }
-      return array;
-    }
-
-    function selected(param) {
-      alert(param);
-    }
-
-    return {
-      number,
-      //answer,
-      msg,
-      options,
-      selected,
-    };
-  },
+let number = ref({
+  a: random(),
+  b: random(),
 });
+const answer = ref(number.value.a * number.value.b);
+const options = ref([]);
+const msg = ref('Escolha sua resposta');
+const selected = ref('');
+
+for (var i = 0; i < 5; i++) {
+  options.value.push(distractors(i));
+}
+options.value.push(answer.value);
+shuffleArray(options.value);
+
+function distractors(i) {
+  if (i < 3) {
+    return answer.value + random();
+  } else {
+    return answer.value - random();
+  }
+}
+
+function shuffleArray(array) {
+  for (let i = 0; i < array.length; i++) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+const onSelected = (param) => {
+  selected.value = param;
+};
 </script>
+
